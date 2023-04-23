25fee153531dfb586554c5cffa3d1ac35b991ae6
beyzoskaya <beyza.kaya@ozu.edu.tr>
2023-04-23 03:12:10 +0300

    sorting algorithms

---
+++ selectionsort.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+using namespace std; 
+  
+void swap(int *xp, int *yp)  
+{  
+    int temp = *xp;  
+    *xp = *yp;  
+    *yp = temp;  
+}  
+  
+void selectionSort(int arr[], int n)  
+{  
+    int i, j, min_idx;  
+  
+    // One by one move boundary of unsorted subarray  
+    for (i = 0; i < n-1; i++)  
+    {  
+        // Find the minimum element in unsorted array  
+        min_idx = i;  
+        for (j = i+1; j < n; j++)  
+            if (arr[j] < arr[min_idx])  
+                min_idx = j;  
+  
+        // Swap the found minimum element with the first element  
+        swap(&arr[min_idx], &arr[i]);  
+    }  
+}  
+  
+// A function to implement bubble sort  
+void bubbleSort(int arr[], int n)  
+{  
+    int i, j;  
+    for (i = 0; i < n-1; i++)      
+      
+    // Last i elements are already in place  
+    for (j = 0; j < n-i-1; j++)  
+        if (arr[j] > arr[j+1])  
+            swap(&arr[j], &arr[j+1]);  
+}  
+
+/* Function to sort an array using insertion sort*/
+void insertionSort(int arr[], int n)  
+{  
+    int i, key, j;  
+    for (i = 1; i < n; i++) 
+    {  
+        key = arr[i];  
+        j = i - 1;  
+  
+        /* Move elements of arr[0..i-1], that are  
+        greater than key, to one position ahead  
+        of their current position */
+        while (j >= 0 && arr[j] > key) 
+        {  
+            arr[j + 1] = arr[j];  
+            j = j - 1;  
+        }  
+        arr[j + 1] = key;  
+    }  
+}  
+  
+/* function to sort arr using shellSort */
+int shellSort(int arr[], int n) 
+{ 
+    // Start with a big gap, then reduce the gap 
+    for (int gap = n/2; gap > 0; gap /= 2) 
+    { 
+        // Do a gapped insertion sort for this gap size. 
+        // The first gap elements a[0..gap-1] are already in gapped order 
+        // keep adding one more element until the entire array is 
+        // gap sorted  
+        for (int i = gap; i < n; i += 1) 
+        { 
+            // add a[i] to the elements that have been gap sorted 
+            // save a[i] in temp and make a hole at position i 
+            int temp = arr[i]; 
+  
+            // shift earlier gap-sorted elements up until the correct  
+            // location for a[i] is found 
+            int j;             
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) 
+                arr[j] = arr[j - gap]; 
+              
+            //  put temp (the original a[i]) in its correct location 
+            arr[j] = temp; 
+        } 
+    } 
+    return 0; 
+} 
+
+/* Function to print an array */
+void printArray(int arr[], int size)  
+{  
+    int i;  
+    for (i=0; i < size; i++)  
+        cout << arr[i] << " ";  
+    cout << endl;  
+}  
+  
+int main()  
+{  
+    int arr[] = {64, 25, 12, 22, 11};  
+    int n = sizeof(arr)/sizeof(arr[0]);  
+    selectionSort(arr, n);  
+    cout << "Selection Sorted array: \n";  
+    printArray(arr, n);  
+
+    int arr2[] = {64, 34, 25, 12, 22, 11, 90};  
+    n = sizeof(arr2)/sizeof(arr2[0]);  
+    bubbleSort(arr2, n);  
+    cout<<"Bubble Sorted array: \n";  
+    printArray(arr2, n);  
+
+    int arr3[] = { 12, 11, 13, 5, 6 };  
+    n = sizeof(arr3) / sizeof(arr3[0]);  
+    insertionSort(arr3, n);  
+    cout<<"Insertion Sorted array: \n";  
+    printArray(arr3, n); 
+
+    int arr4[] = {12, 34, 54, 2, 3}; 
+    n = sizeof(arr4)/sizeof(arr4[0]); 
+    cout << "Array before sorting: \n"; 
+    printArray(arr4, n); 
+    shellSort(arr4, n); 
+    cout << "Array after shell sorting: \n"; 
+    printArray(arr4, n); 
+
+    return 0;  
+} 
